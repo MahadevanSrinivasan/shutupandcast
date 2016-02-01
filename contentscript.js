e594f4c37e404ee362e8206e80c709d2bd54fa35
MahadevanSrinivasan <macsdev@gmail.com>
2016-01-31 16:16:58 -0800

    add vidzi support

--- contentscript.js
+++ contentscript.js
@@ -27,6 +27,7 @@ function findVideoLink ()
   var vodmine = /vodmine/;
   var ishared = /ishared/;
   var thevideo = /thevideo/;
+  var vidzi = /vidzi/;
   
   if(allmyv.test(url))
       regex = /"file" : "(.+?)"/;
@@ -37,7 +38,9 @@ function findVideoLink ()
   else if(vodmine.test(url))
       regex = /<source src="(.+?\.mp4)"/;
   else if(ishared.test(url))
-      regex = /path:"(.+?)"/; 
+      regex = /path:"(.+?)"/;
+  else if(vidzi.test(url))
+      regex = /file: "(.+?\.mp4)"/; 
   else if(thevideo.test(url))
       regex = /label: '480p', file: '(.+?)'/;
   // Test the text of the body element against our regular expression.
